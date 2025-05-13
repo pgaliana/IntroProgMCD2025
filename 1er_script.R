a80a6c228d42675230d2953405c141fa863f0ae5
Pablo Galiana <pgaliana@gmail.com>
2025-05-13 19:54:17 -0300

    Create 1er_script.R
    aa

---
+++ 1er_script.R
@@ -0,0 +1,20 @@
+# 1er_script.R
+
+rm(list = ls())
+options(scipen = 999)
+
+# Obtener directorio del script actual y modificar el working directory
+# install.packages(rstudioapi)
+library("rstudioapi")
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+getwd()
+
+# ----------------------------------------
+# Módulo 3: Programación en R
+# ----------------------------------------
+
+# 1. Estructuras de control
+# -------------------------
+
+# Ejemplo 1: Estructura condicional if-else
+print("Ejemplo de Script de R")
